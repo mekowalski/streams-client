62a6956b4c3d8985995ed74a936ef2680c97cdeb
Malind Kowalski <malind.nikulski@gmail.com>
2019-05-16 18:22:50 -0600

    this is before refactor with link, includes anchor tags, route comps and paths

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,36 @@
 import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 
+const PageOne = () => {
+  return (
+    <div>
+      Page One
+      <a href='/pagetwo'>Click Page2</a>
+    </div>
+  )
+}
+
+const PageTwo = () => {
+  return (
+    <div>
+      Page Two
+      <a href='/pageone'>Click Page1</a>
+    </div>
+  )
+}
+
 const App = () => {
   return (
     <div>
       Welcome to the Streams App!
+      <div>
+        <BrowserRouter>
+          <div>
+            <Route path='/' exact component={PageOne}/>
+            <Route path='/pagetwo' component={PageTwo} />
+          </div>
+        </BrowserRouter>
+      </div>
     </div>
   )
 }
