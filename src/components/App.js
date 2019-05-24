754c706813c37ef6ff5e715afea71818c8009426
Malind Kowalski <malind.nikulski@gmail.com>
2019-05-24 14:57:30 -0600

    wire up actual routes

--- src/components/App.js
+++ src/components/App.js
@@ -13,7 +13,11 @@ const App = () => {
       <div>
         <BrowserRouter>
           <div>
-
+            <Route path='/' exact component={StreamList} />
+            <Route path='/streams/new' exact component={StreamCreate} />
+            <Route path='/streams/edit' exact component={StreamEdit} />
+            <Route path='/streams/delete' exact component={StreamDelete} />
+            <Route path='/streams/show' exact component={StreamShow} />
           </div>
         </BrowserRouter>
       </div>
