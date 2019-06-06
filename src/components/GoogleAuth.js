ab23a943283d4bf47780d91c2b0f5e9b9fde8aa7
Malind Kowalski <malind.nikulski@gmail.com>
2019-06-06 17:12:00 -0600

    remove comments

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,27 +1,22 @@
 import React from 'react';
 
 class GoogleAuth extends React.Component {
-  //initialize state object when component class is created
   state = { isSignedIn: null }
 
   componentDidMount() {
     window.gapi.load('client:auth2', () => {
-      window.gapi.client.init({ //this executes an asynchronous request/operation in order to init client
+      window.gapi.client.init({
         clientId: '1069443773373-jk72j3l7ov6ad9snjob9fm6hiphf065d.apps.googleusercontent.com',
         scope: 'email'
-        //want a notice when initialization is all done
-        //chain on .then statement
+
       })
       .then(() => {
-        //1. ref to auth object and save ref on component class
         this.auth = window.gapi.auth2.getAuthInstance()
-        //2. figure out if user is currently signed in
         this.setState({ isSignedIn: this.auth.isSignedIn.get() })
       })
     })
   }
 
-  //helper method instead of using logic within render()
   renderAuthButton() {
     if (this.state.isSignedIn === null) {
       return <div> i dont know if i am signed in </div>
