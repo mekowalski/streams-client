bd9e0e6ed834e41d876cf2d04248bbe7674a7ea7
Malind Kowalski <malind.nikulski@gmail.com>
2019-06-21 13:08:05 -0600

    action creators completed

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,17 @@
 //want to eventually have 2 action creators: signIn() and signOut()
 //very straighforward, only called either after user has successfully logged in/out through GAPI library
+
+//eventually have reducer that will see an action type SIGN_IN, it will probably have internal boolean flag
+//to update sign in status to true
+export const signIn = () => {
+  return {
+    type: 'SIGN_IN'
+  }
+}
+
+//same idea with sign out
+export const signOut = () => {
+  return {
+    type: 'SIGN_OUT'
+  }
+}
