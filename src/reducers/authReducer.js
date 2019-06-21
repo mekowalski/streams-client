0a18d59fe3cf82d73436ff802cf596c66340d6f8
Malind Kowalski <malind.nikulski@gmail.com>
2019-06-21 13:42:11 -0600

    add switch statement for authReducer

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,4 +1,5 @@
 //in order to default state argument OR in order to initialize it, create an initializer
+//caps because this is supposed to be a true constant object, never change the value of it
 const INITIAL_STATE = {
   isSignedIn: null
 }
@@ -6,6 +7,15 @@ const INITIAL_STATE = {
 //function gets called with default state object
 //idea of default argument
 //when app first goes, reducer gets called 1 time to initialize it
+//switch statement, update/modify state object with spread syntax
+//and update isSignedIn/isSignedOut properties as well
 export default (state = INITIAL_STATE, action) => {
-
+  switch (action.type) {
+    case 'SIGN_IN':
+      return { ...state, isSignedIn: true }
+    case 'SIGN_OUT':
+      return { ...state, isSignedOut: false}
+    default:
+      return state
+  }
 }
