3f6aa3a45dc1d43415b2baa0f63fbd79b141b68a
Malind Kowalski <malind.nikulski@gmail.com>
2019-06-22 15:35:44 -0600

    default userId, update userID

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,9 +2,10 @@ import { SIGN_IN, SIGN_OUT } from './types';
 
 //need to make sure the action creator receives the user ID as an argument
 //also pass to a Reducer, by assigning ID to action object as a Payload property
-export const signIn = () => {
+export const signIn = (userId) => {
   return {
-    type: SIGN_IN
+    type: SIGN_IN,
+    payload: userId
   }
 }
 
--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,15 +1,18 @@
 import { SIGN_IN, SIGN_OUT } from '../actions/types';
 
 const INITIAL_STATE = {
-  isSignedIn: null
+  isSignedIn: null,
+  userId: null
 }
 
+//finally, when a user signs in, not only update isSignedIn property but also update user ID flag
+//ALSO want to clear userId property when a user signs out
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case SIGN_IN:
-      return { ...state, isSignedIn: true }
+      return { ...state, isSignedIn: true, userId: action.payload }
     case SIGN_OUT:
-      return { ...state, isSignedIn: false}
+      return { ...state, isSignedIn: false, userId: null }
     default:
       return state
   }
