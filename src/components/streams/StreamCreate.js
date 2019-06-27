946036f64fd363294f7df5f82c35a7c357709731
Malind Kowalski <malind.nikulski@gmail.com>
2019-06-26 20:59:51 -0600

    explain what reduxForm gives with props

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import { Field, reduxForm } from 'redux-from';
+import { Field, reduxForm } from 'redux-form';
 
+//now that reduxForm is hooked up, this component is now going to be passed a ton of additional props
+//props that weren't passed before
 class StreamCreate extends React.Component {
   render() {
+    console.log(this.props)
+    //props object has massive amount of properties thanks to the reduxForm helper
+    //up to me to pick out which props are relevant to build the form
     return (
       <div>
       here is the stream create page
