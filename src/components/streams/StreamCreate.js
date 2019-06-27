ba75dd7e1c76887de244357ee7a0046377867e7e
Malind Kowalski <malind.nikulski@gmail.com>
2019-06-27 16:39:23 -0600

    refactor input elements for now, i want to revert back to original method though

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,15 +2,23 @@ import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends React.Component {
-  renderInput(formProps, label) {
+  // renderInput(formProps, label) {
+  //   return (
+  //     <div className='field'>
+  //       <label>{label}</label>
+  //       <input
+  //         onChange={formProps.input.onChange}
+  //         value={formProps.input.value}
+  //       />
+  //     <div>
+  //   )
+  // }
+  renderInput({ input, label }) {
     return (
-      <div className='field'>
-        <label>{label}</label>
-        <input
-          onChange={formProps.input.onChange}
-          value={formProps.input.value}
-        />
       <div>
+        <label>{label}</label>
+        <input {...input} />
+      </div>
     )
   }
 
