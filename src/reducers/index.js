1262833e7dd72d487bbc9b274f5971ac8b5b2e67
Malind Kowalski <malind.nikulski@gmail.com>
2019-06-26 20:42:54 -0600

    rename reducer and move onto next task

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
+//helpful differentiation of other reducers, frequently rename the named export when pulled into file
+//rename via: as
 import { reducer as formReducer } from 'redux-form';
 import authReducer from './authReducer';
 
 export default combineReducers({
   auth: authReducer,
   //required to wire up the Reducer to a key of form
-  form: reducer
+  form: formReducer
 })
