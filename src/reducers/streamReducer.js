80ed8e29d21f8cadeaddbb51ab9df5aa50317271
Malind Kowalski <malind.nikulski@gmail.com>
2019-07-09 11:02:07 -0600

    import lodash, add case statement for DELETE_STREAM and explain

--- src/reducers/streamReducer.js
+++ src/reducers/streamReducer.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import {
   FETCH_STREAM, FETCH_STREAMS, CREATE_STREAM, EDIT_STREAM, DELETE_STREAM
 } from '../actions/types';
@@ -11,6 +12,11 @@ export default (state = {}, action) => {
       return { ...state, [action.payload.id]: action.payload }
     case EDIT_STREAM:
       return { ...state, [action.payload.id]: action.payload }
+    case DELETE_STREAM:
+      return _.omit(state, action.payload)
+      //return omit with entire State object, and what key that needs to be deleted from the State object
+      //in this case it's action.payload
+      //don't need .id because when the action dispatches a type of DELETE_STREAM, the payload is the ID
 
     default:
       return state
