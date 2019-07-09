869d90478d0dd19845e76ac1c81f71f1feede314
Malind Kowalski <malind.nikulski@gmail.com>
2019-07-09 10:13:35 -0600

    export REST actions and add boilerplate for reducer

--- src/reducers/streamReducer.js
+++ src/reducers/streamReducer.js
@@ -1,3 +1,12 @@
 import {
-
+  FETCH_STREAM, FETCH_STREAMS, CREATE_STREAM, EDIT_STREAM, DELETE_STREAM
 } from '../actions/types';
+
+
+export defautl (state = {}, action) => {
+  switch (action.type) {
+
+    default:
+      return state
+  }
+}
