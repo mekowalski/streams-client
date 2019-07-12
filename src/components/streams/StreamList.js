78d56ebbb6efa4561f600692b2e5b35fe9db9a42
Malind Kowalski <malind.nikulski@gmail.com>
2019-07-12 10:22:33 -0600

    refactor from functional to class-based component

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -2,12 +2,17 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { fetchStreams } from '../../actions';
 
-const StreamList = () => {
-  return (
-    <div>
-      here is the StreamList page
-    </div>
-  )
+//replace this with class-based component
+//want to call action creator inside componentDidMount()
+//attempt to fetch list of streams only once
+class StreamList extends React.Component {
+  render() {
+    return (
+      <div>
+        this is the stream list
+      </div>
+    )
+  }
 }
 
 export default connect(null, { fetchStreams })(StreamList)
